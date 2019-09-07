2d80fa3a783e37908759f16ec68f087d4b4c3f28
kkAyataka <kk.ayataka@gmail.com>
2019-09-07 15:53:34 +0900

    Import hello-codeceptjs

---
+++ hello-codeceptjs/codecept.conf.js
@@ -0,0 +1,17 @@
+exports.config = {
+  tests: './*_test.js',
+  output: './output',
+  helpers: {
+    WebDriver: {
+      url: 'http://localhost',
+      browser: 'chrome'
+    }
+  },
+  include: {
+    I: './steps_file.js'
+  },
+  bootstrap: null,
+  mocha: {},
+  name: 'hello-codeceptjs',
+  translation: 'ja-JP'
+}
